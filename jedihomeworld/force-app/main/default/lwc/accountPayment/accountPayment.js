5f47dbf47f2bf14023d90a9c90932398cfd6488c
Warren Walters <wwalt010@fiu.edu>
2019-06-06 22:58:00 -0400

    LWC Void button working

--- jedihomeworld/force-app/main/default/lwc/accountPayment/accountPayment.js
+++ jedihomeworld/force-app/main/default/lwc/accountPayment/accountPayment.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire, track } from 'lwc';
-import getAccountList from '@salesforce/apex/AccountPaymentController.getAccountList';
-
+import  getAccountList from '@salesforce/apex/AccountPaymentController.getAccountList';
+import voidPayments from '@salesforce/apex/AccountPaymentController.voidPayments';
+import { refreshApex } from '@salesforce/apex';
 
 export default class AccountPayment extends LightningElement {
 
@@ -8,33 +9,37 @@ export default class AccountPayment extends LightningElement {
         @track error;
         @track columns = [
             { label: 'Name', fieldName: 'Name'},
-            { label: '# of Payments', fieldName: 'Number_of_Non_void_Payments__c'},
-            {type: "button", typeAttributes: {
-                label: 'Edit',
-                name: 'Edit',
-                title: 'Edit',
+            { label: '# of Payments', fieldName: 'Number_of_Non_void_Payments__c', type : 'number'},
+            { label: 'Void Payments', type: "button", typeAttributes: {
+                label: 'Void',
+                name: 'Void',
+                title: 'Void',
                 disabled: false,
-                value: 'edit',
-                iconPosition: 'left'
+                value: 'void',
+                iconPosition: 'center'
             }}
         ];
 
+        accountData; 
+
+        //get account list
         @wire(getAccountList)
-        wireAccounts({
-            error,
-            data
-        }) {
-            if(data){
-                this.data = data;
-                // eslint-disable-next-line no-console
-                console.log(data);
-                // eslint-disable-next-line no-console
-                console.log(JSON.stringify(data, null, '\t'));
-            } else if (error){
-                this.error = error;
+        wireAccounts(result){
+            this.accountData = result;
+            if (result.data) {
+                this.data = result.data;
+                this.error = undefined;
+            } else if (result.error) {
+                this.error = result.error;
+                this.accounts = undefined;
             }
         }
 
-        
-
+        //button void press void all payments
+        handleRowAction(event) {
+            const row = event.detail.row;
+            voidPayments({accountId : row.Id}).then(() => {
+                return refreshApex(this.accountData);
+            });
+        }
 }
